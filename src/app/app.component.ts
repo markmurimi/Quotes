caab9934a5d3bfa9f7ff5af09bf4212e0852c1f0
markmurimi <murimimg180@gmail.com>
2018-03-17 15:40:39 +0300

    commit changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { Quote} from './quote'
 })
 export class AppComponent {
   quotes = [
-    new Quote(1, 'Happy coding', 'Always be happy when coding'),
-    new Quote(2, 'Angular js', 'On my point of view angular is a bit complex'),
+    new Quote(1, '"I love you the more in that I believe you had liked me for my own sake and for nothing else."', 'AUTHOR:John Keats SUBMITED BY:Mark Murimi'),
+    new Quote(2, '"But man is not made for defeat. A man can be destroyed but not defeated."', 'AUTHOR:Ernest Hemingway SUBMITED BY:Ramza Reseni'),
   ]
   }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,16 +9,19 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    new Quote(1, 'Happy coding','Always be happy when coding'),
-    new Quote(2, 'Angular js', 'On my point of view angular is a bit complex'),
+    new Quote(1, '"I love you the more in that I believe you had liked me for my own sake and for nothing else."', 'AUTHOR:John Keats SUBMITED BY:Mark Murimi'),
+    new Quote(2, '"But man is not made for defeat. A man can be destroyed but not defeated."', 'AUTHOR:Ernest Hemingway     SUBMITED BY:Ramza Reseni'),
   ]
 
   togleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
-  completeQuote(isComplete,index){
+  deleteQuote(isComplete,index){
     if (isComplete){
-      this.quotes.splice(index,1)
+      let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
+      if(toDelete){
+        this.quotes.splice(index,1)
+      }
     }
   }
   constructor() { }
