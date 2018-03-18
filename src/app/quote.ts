d9cd80af22289d6660856200a3d76ce4daa1bb2d
markmurimi <murimimg180@gmail.com>
2018-03-18 13:47:07 +0300

    modified quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showDescription:boolean;
-    constructor(public id:number, public name:string, public description:string){
+    constructor(public id:number, public name:string, public description:string,public creationDate:Date){
         this.showDescription=false
     }
 }
